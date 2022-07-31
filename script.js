6f2dd3854dade03ebeec3a3e2b1c59aa490183ab
Xephna <36911354+Xephna@users.noreply.github.com>
2022-07-31 21:08:05 +0800

    Add basic markup and styling

---
+++ script.js
@@ -0,0 +1,11 @@
+// variables
+const body = document.body;
+let ratingContainer = document.getElementsByClassName('rating-container');
+const thankYouContainer = document.getElementsByClassName('thankyou-container');
+const submitBtn = document.getElementsByClassName('submit-btn');
+
+console.log(ratingContainer, thankYouContainer, submitBtn);
+
+function showThankYouWindow () {
+    
+}

14a2a39d21efecbd4f3f77e10e0fce5ad3283b85
Xephna <36911354+Xephna@users.noreply.github.com>
2022-08-01 17:29:09 +0800

    Complete desktop solution

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 // variables
-const body = document.body;
-let ratingContainer = document.getElementsByClassName('rating-container');
-const thankYouContainer = document.getElementsByClassName('thankyou-container');
-const submitBtn = document.getElementsByClassName('submit-btn');
+let ratingContainer = document.getElementById('rating-container');
+let thankYouContainer = document.getElementById('thank-you-container');
+let submitBtn = document.getElementById('submit-btn');
+let ratingText = document.getElementById('rating');
+let rating = 0;
 
-console.log(ratingContainer, thankYouContainer, submitBtn);
+submitBtn.addEventListener('mouseup', ()=>{
+    ratingContainer.style.display = 'none';
+    thankYouContainer.style.display = 'block';
+    ratingText.innerText='You selected '+rating+' out of 5';
+})
 
-function showThankYouWindow () {
-    
+for (let i = 1; i < 6; i++){
+    let score = document.getElementById('rate-'+i);
+    score.addEventListener('mouseup', ()=>{
+        rating = i;
+        score.style.backgroundColor = 'hsl(217, 12%, 63%)';
+        score.style.color = 'white';
+        for (let j = 1; j < 6; j++){
+            if (j !== rating){
+                document.getElementById('rate-'+j).style.backgroundColor = 'hsl(216, 17%, 23%)';
+                document.getElementById('rate-'+j).style.color = 'hsl(217, 12%, 63%)';
+            }
+        }
+    })
+    score.addEventListener('mousedown', ()=>score.style.backgroundColor= 'hsl(25, 97%, 53%)')
 }
